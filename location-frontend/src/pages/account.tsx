ac7c6bdbd5e6775f36ddfa7cd74146264ee8f5b7
Jorge <jorge.mauro@localiza.com>
2021-02-14 23:16:38 -0300

    Consumo dos dados disponibilizados pelo nestjs

--- location-frontend/src/pages/account.tsx
+++ location-frontend/src/pages/account.tsx
@@ -1,14 +1,35 @@
 import * as React from 'react'
+import { GetServerSideProps } from "next";
 import Layout from "../components/Layout";
 import Title from "../components/Title";
-const UserAccount =()=>{
+import { bankHttp } from "../util/http";
+const UserAccount =(props)=>{
+  const { userAccounts } = props;
     return(<Layout>
         <Title>Sua conta</Title>
         <div className="row">
-          Teste Inicial
+        {userAccounts&&<div className="card">
+            <h5 className="card-title">{userAccounts[0].name}</h5>
+            <div className="card-body">
+              <ul className="list-group">
+                <li className="list-group-item"><b>CPF</b>:{userAccounts[0].cpf}</li>
+                <li className="list-group-item"><b>matricula</b>:{userAccounts[0].matricula}</li>
+                <li className="list-group-item"><b>aniversario</b>:{userAccounts[0].birth}</li>
+              </ul>
+            </div>
+          </div>}
         </div>
       </Layout>)
     
     }
 
     export default UserAccount
+
+    export const getServerSideProps: GetServerSideProps = async () => {
+      const { data: userAccounts } = await bankHttp.get("user-accounts/userAccount");
+      return {
+        props: {
+          userAccounts,
+        },
+      };
+    };
